5634f009295dae890c8ed7ec173cece6dcd512d3
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-06-24 20:02:49 +0530

    added more data types and it's explanation

--- ch2-javascript_datatypes/script.js
+++ ch2-javascript_datatypes/script.js
@@ -6,5 +6,35 @@
 // So, there is a typeof() operator in JavaScript which determins the data type of a variable.
 
 //example -
-const age = 26;
-console.log(typeof age);
+// const prompt_age = prompt(" Please Enter Your Age = "); // we used prompt() to take input from the user in the browser.
+// const age = parseInt(prompt_age); // Converted prompt_age to interger using parseInt().
+
+const age = 32;
+
+// There are 6 Data Types in JavaScript which are primitive types-
+// *undefined : typeof instance === "undefined"
+// *Boolean : typeof instance === "boolean"
+// *Number : typeof instance === "number"
+// *String : typeof instance === "string"
+// *BigInt : typeof instance === "bigint"
+// *Symbol : typeof instance === "Symbol"
+
+// The entire course documented is from Thapa technical Javascript Video and it either follows ES20 or ES21.
+
+if (typeof age === "number") {
+	console.log(" Your Data Type is number!");
+} else {
+	console.log(" Your Data Type is not number!");
+}
+
+// *** Important ***
+
+// When you add any number with any string then it will concatenate and display but in case of
+// substracted, it will do the mathematical operation. Example -
+
+console.log(9 + "6"); // The output is concatinated string
+console.log(9 + "Car"); // The output is concatinated string
+// But when we do substraction with a number inside "" which makes it string, it works and does the mathematical operation -
+console.log(200 - "12"); // The output is mathematical operation which is 118.
+console.log(200 - "raju"); // The output is NaN which means not a number.
+// This is because (maybe) when javascript tried to substract, it fould it not a number and returned NaN.
