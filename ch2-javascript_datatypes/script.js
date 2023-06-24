abb8705eeadd9de11062f83fa809cb9580bfffb2
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-06-24 14:19:19 +0530

    new chapter content added

---
+++ ch2-javascript_datatypes/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// In Javascript, we exclusively do not define the data type of the variable seperately.
+// It is because it can automatically determine the data type of a variable.
+// Which can be a problem for a begonner to understand and in some cases for the experienced developer too.
+// So, there is a typeof() operator in JavaScript which determins the data type of a variable.
+
+//example -
+const age = 26;
+console.log(typeof age);
