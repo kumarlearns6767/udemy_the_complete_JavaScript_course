cc1cffc454aa4fcd50e4aa24b2e5f9dae4b45942
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-06-30 21:01:30 +0530

    updated doubt.js content

--- doubt.java
+++ doubt.java
@@ -4,5 +4,9 @@ public class doubt {
         System.out.println("\n\n This File is to log the doubts in entire Javascript Course.");
         System.out.println("It is intentionally created in Java to showcase basic level of java skills");
         System.out.println("Doubts are in the following chapters- ");
+        System.out.println("\n");
+        System.out.println("Chapter 2 - line no 84 onwards.");
+        System.out.println("\n");
+        System.out.println("Chapter 3 - line no 44");
     }
 }
