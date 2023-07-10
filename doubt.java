6af133735b291fd9b0b2c8a4191e288a3eef5a18
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-07-10 10:13:23 +0530

    updated doubt.java file

--- doubt.java
+++ doubt.java
@@ -1,18 +1,14 @@
 import java.io.*;
 public class doubt {
     public static void main(String[] args)throws IOException {
-        // System.out.println("\n\n This File is to log the doubts in entire Javascript Course.");
-        // System.out.println("\n It is intentionally created in Java to showcase basic level of java skills");
-        // System.out.println("Doubts are in the following chapters- ");
-        // System.out.println("\n");
-        // System.out.println("Chapter 2 - line no 84 onwards.");
-        // System.out.println("\n");
-        // System.out.println("Chapter 3 - line no 44");
-        // System.out.println("\n");
-        // System.out.println("Chapter 3 - line no 54");
-
-        double sum = 1.2;
-        sum = sum +1;
-        System.out.println("\n\nNew Value of Sum = "+sum);
+        System.out.println("\n\n This File is to log the doubts in entire Javascript Course.");
+        System.out.println("\n It is intentionally created in Java to showcase basic level of java skills");
+        System.out.println("Doubts are in the following chapters- ");
+        System.out.println("\n");
+        System.out.println("Chapter 2 - line no 84 onwards.");
+        System.out.println("\n");
+        System.out.println("Chapter 3 - line no 44");
+        System.out.println("\n");
+        System.out.println("Chapter 3 - line no 54");
     }
 }
