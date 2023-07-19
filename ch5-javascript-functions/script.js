48b7104bc613a4375f279cbd4483bd0ea80aadcd
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-07-19 20:33:28 +0530

    added function chapter content

--- ch5-javascript-functions/script.js
+++ ch5-javascript-functions/script.js
@@ -2,8 +2,19 @@
 
 // alert("Chapter 4 page is incomplete so please do not click \n on any of the program links as it may not work as expected. \n the Issue willbe fixed when lalit lands a job.");
 
-
 // A JavaScript function is a block of code
 // designed to perform a particular task.
 // A JavaScript function is executed when
 // "something" invokes it (calls it).
+
+// Example -
+
+function sum() {
+    let a = 12,
+        b = 13,
+        c;
+    c = a + b;
+    console.log(`Sum of ${a} and ${b} = ${c}`);
+}
+
+sum();
