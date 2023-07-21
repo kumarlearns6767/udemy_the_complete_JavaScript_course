598e6949f653923632cdbedd110a8dc898db26e4
lalitkumarthakur <lkt929129@gmail.com>
2023-07-21 15:13:33 +0530

    added ecmascript content

---
+++ ch6-ecmaScript/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Javascript Ecma Script.
+
+console.log(`Welcome to Javascript Ecma Script !`);
+console.log(`\n Javascript was created in 1996,`);
+console.log(`In 1997, Javascript was submitted to ECMA International,`);
+console.log(`In 2015, ECMAScript decided to switch to regular release and ES6 was formed.`);
+
+
