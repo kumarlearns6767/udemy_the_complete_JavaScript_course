4ff5402bda54c29067edafa5de4b95ec7b196b8e
lalitkumarthakur <lkt929129@gmail.com>
2023-07-21 15:15:28 +0530

    added ecmascript6 content

--- ch6-ecmaScript/script.js
+++ ch6-ecmaScript/script.js
@@ -7,4 +7,6 @@ console.log(`\n Javascript was created in 1996,`);
 console.log(`In 1997, Javascript was submitted to ECMA International,`);
 console.log(`In 2015, ECMAScript decided to switch to regular release and ES6 was formed.`);
 
+// 2015 => ECMAScript 2015 or ES6 (6th Edition).
+// 
 
