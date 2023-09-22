135602d1573b4a47115bd300f4f0070facd63440
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-09-22 21:39:24 +0530

    Updated ch4 program 2

--- ch4-javascript-condition/if-else-programs/p2-leapYear/LeapYear.js
+++ ch4-javascript-condition/if-else-programs/p2-leapYear/LeapYear.js
@@ -2,17 +2,17 @@
 
 function leapYear() {
 	let year = document.getElementById("yearbyUser").value;
-	console.log(`The Year you have enteres is = ${year}`);
+	console.log(`The Year you have entered is = ${year}`);
 
 	if (isNaN(year)) {
 		alert("Please enter a Year as a Number and Try again! ");
 	} else {
+		// if year is divisible by 4 then leap year.
 		if (year % 4 === 0) {
-			// if year is divisible by 4 then leap year.
+			// if year is divisible by 4 then not a leap year.
 			if (year % 100 === 0) {
-				// if year is divisible by 4 then not a leap year.
+				// if year is divisible by 400 then leap year.
 				if (year % 400 === 0) {
-					// if year is divisible by 400 then leap year.
 					document.getElementById("yearReturn").innerText = "Your Number is a Leap Year !";
 				} else {
 					document.getElementById("yearReturn").innerText = "Your Number is not a Leap Year !";
