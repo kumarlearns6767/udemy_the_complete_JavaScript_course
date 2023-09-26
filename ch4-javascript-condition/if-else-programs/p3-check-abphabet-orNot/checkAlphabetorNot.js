7877b46345401ebed940e3815ae25a8456f34c9d
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-09-27 04:27:29 +0530

    solved ch4 p3 if else

--- ch4-javascript-condition/if-else-programs/p3-check-abphabet-orNot/checkAlphabetorNot.js
+++ ch4-javascript-condition/if-else-programs/p3-check-abphabet-orNot/checkAlphabetorNot.js
@@ -5,20 +5,31 @@ function alphabet() {
     document.getElementById('error-text').innerHTML = "";
 
     // Clear the result message
-    document.getElementById("return-value").innerText = "Your Number is =";
+    document.getElementById("return-value").innerText = "";
 
     let str = document.getElementById("yearbyUser").value;
     console.log(`The Year you have entered is = ${str}`);
 
+    const errorText = document.getElementById('error-text');
+
     if (!isNaN(str) || str == "" || str === "") {
-        const errorText = document.getElementById('error-text');
-        errorText.innerHTML = "Invalid Entry !!!  Please Enter a Number and try Again !";
+        errorText.innerHTML = "Invalid input. Please enter a single alphabet character !!!";
         document.getElementById("return-value").innerText = "";
         errorText.classList.remove('error-text'); // Remove the class
         setTimeout(() => {
             errorText.classList.add('error-text'); // Add the class to trigger the animation
         }, 0); // Use a small delay to ensure the class is removed and re-added
     } else {
-
+        if (str.length === 1 && (str >= 'a' && str <= 'z' || str >= 'A' && str <= 'Z')) {
+            document.getElementById("error-text").innerText = ""; // Clear any previous error message
+            document.getElementById("return-value").innerText = str + " is an alphabet ";
+        } else {
+            document.getElementById("return-value").innerText = ""; // Clear any previous result
+            document.getElementById("error-text").innerText = "Invalid input. Please enter a single alphabet character !!!";
+            errorText.classList.remove('error-text'); // Remove the class
+            setTimeout(() => {
+                errorText.classList.add('error-text'); // Add the class to trigger the animation
+            }, 0);
+        }
     }
 }
