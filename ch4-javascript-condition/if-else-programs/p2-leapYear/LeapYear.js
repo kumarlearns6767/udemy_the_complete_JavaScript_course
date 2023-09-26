c602bead9ea3e120a25ade54073f9b4f6d1b85d5
lalitkumarthakur <lkt929129@gmail.com>
2023-09-26 15:28:01 +0530

    updated ch4 program 3 if else

--- ch4-javascript-condition/if-else-programs/p2-leapYear/LeapYear.js
+++ ch4-javascript-condition/if-else-programs/p2-leapYear/LeapYear.js
@@ -1,11 +1,23 @@
 "use strict";
 
 function leapYear() {
+	// Clear any previous error message
+	document.getElementById('error-text').innerHTML = "";
+
+	// Clear the result message
+	document.getElementById("return-value").innerText = "Your Number is =";
+
 	let year = document.getElementById("yearbyUser").value;
 	console.log(`The Year you have entered is = ${year}`);
 
-	if (isNaN(year)) {
-		alert("Please enter a Year as a Number and Try again! ");
+	if (isNaN(year) || year == "" || year === "") {
+		const errorText = document.getElementById('error-text');
+		errorText.innerHTML = "Invalid Entry !!!  Please Enter a Number and try Again !";
+		document.getElementById("return-value").innerText = "";
+		errorText.classList.remove('error-text'); // Remove the class
+		setTimeout(() => {
+			errorText.classList.add('error-text'); // Add the class to trigger the animation
+		}, 0); // Use a small delay to ensure the class is removed and re-added
 	} else {
 		// if year is divisible by 4 then leap year.
 		if (year % 4 === 0) {
@@ -13,15 +25,15 @@ function leapYear() {
 			if (year % 100 === 0) {
 				// if year is divisible by 400 then leap year.
 				if (year % 400 === 0) {
-					document.getElementById("yearReturn").innerText = "Your Number is a Leap Year !";
+					document.getElementById("return-value").innerText = "Your Number is a Leap Year !";
 				} else {
-					document.getElementById("yearReturn").innerText = "Your Number is not a Leap Year !";
+					document.getElementById("return-value").innerText = "Your Number is not a Leap Year !";
 				}
 			} else {
-				document.getElementById("yearReturn").innerText = "Your Number is a Leap Year !";
+				document.getElementById("return-value").innerText = "Your Number is a Leap Year !";
 			}
 		} else {
-			document.getElementById("yearReturn").innerText = "Your Number is not a Leap Year !";
+			document.getElementById("return-value").innerText = "Your Number is not a Leap Year !";
 		}
 	}
 }
