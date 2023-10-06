261d06d3ef94cc22b462aa8fef4920ccb9c72987
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-10-06 19:52:29 +0530

    added 1 program in while loop

---
+++ ch5-javascript-loops/while-loop-programs/printAll_EvenNo_from1to100.js
@@ -0,0 +1,21 @@
+// Write a program to print all even numbers between 1 to 100
+
+'use strict';
+
+// let i;
+// for (i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`${i} is an Even number`);
+//     }
+// }
+
+// while loop - 
+
+
+let j = 1;
+while (j <= 100) {
+    if (j % 2 == 0) {
+        console.log(`${j} is an Even number`);
+    }
+    j++;
+}
