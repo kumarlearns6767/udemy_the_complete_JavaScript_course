eda182c2a357f783633bb60984af191670f662cb
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-10-11 13:57:14 +0530

    test

--- ch6-javascript-functions/script.js
+++ ch6-javascript-functions/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Javascript Function
+
 // alert("Chapter 4 page is incomplete so please do not click \n on any of the program links as it may not work as expected. \n the Issue willbe fixed when lalit lands a job.");
 
 // A JavaScript function is a block of code
