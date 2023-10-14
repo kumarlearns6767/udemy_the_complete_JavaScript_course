5db40a793a215d722d06df9ce5fc329c23d19195
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-10-15 01:55:38 +0530

    test

---
+++ ch6-javascript-functions/programs/checkAlphabet/checkAlphabet_orNot.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let userinput = document.getElementById("UserInput"); // Selecting User Input Input box
+userinput.value = ""; // Selecting the inputbox value and making it to Null / ""
+
+let errorText = document.getElementById("error-text");
+errorText.innerHTML = "";
+let resultMessage = document.getElementById("resultMessage");
+resultMessage.innerHTML = "";
