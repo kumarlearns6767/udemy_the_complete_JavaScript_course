91251a4295f7ff27771d535b89216215c6b4b776
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-10-18 15:25:24 +0530

    Check alphabelt using function program added with bug

--- ch6-javascript-functions/programs/checkAlphabet/checkAlphabet_orNot.js
+++ ch6-javascript-functions/programs/checkAlphabet/checkAlphabet_orNot.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Write a Program in Javascript to check that weather a single character is Alphabet or not using Parameterized Function.
+
 let userinput = document.getElementById("UserInput"); // Selecting User Input Input box
 userinput.value = ""; // Selecting the inputbox value and making it to Null / ""
 
@@ -7,3 +9,21 @@ let errorText = document.getElementById("error-text");
 errorText.innerHTML = "";
 let resultMessage = document.getElementById("resultMessage");
 resultMessage.innerHTML = "";
+
+function checkAlphabet() {
+    debugger;
+    if (userinput.value === "") {
+        errorText.innerHTML = "Entry cannot be empty. Please enter a character! and try again ";
+    }
+    else if (userinput.value.length !== 1) {
+        resultMessage.innerHTML = "";
+        errorText.innerHTML = "Please enter a Single Character and try again !";
+    } else if ((userinput.value >= 'a' || userinput.value <= 'z') || (userinput.value >= 'A' || userinput.value <= 'Z')) {
+        errorText.innerHTML = "";
+        resultMessage.innerHTML = "You have entered a Alphabet !";
+
+    } else {
+        resultMessage.innerHTML = "";
+        errorText.innerHTML = "Invalid Entry! Please try again !";
+    }
+}
