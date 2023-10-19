d467ca710a72f9f56dff472a123ceb33f5cf4372
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-10-19 16:11:52 +0530

    completed halp default parameters in es6 ch7 point 18.

--- ch7-ecmaScript/script.js
+++ ch7-ecmaScript/script.js
@@ -12,37 +12,38 @@ console.log(`In 2015, ECMAScript decided to switch to regular release and ES6 wa
 
 // New Features in ES6 -
 
-// The let keyword
-// The const keyword
-// TEMPLATE STRINGS
-// Arrow Functions
-// DESTRUCTURING
-// OBJECT PROPERTIES
-// DEFAULT ARGUMENTS
-// REST OPERATORS
-// SPREAD OPERATORS
-// The ... Operator
-// For/of
-// Map Objects
-// Set Objects
-// Classes
-// Promises
-// Symbol
-// Default Parameters
-// Function Rest Parameter
-// String.includes()
-// String.startsWith()
-// String.endsWith()
-// Array.from()
-// Array keys()
-// Array find()
-// Array findIndex()
-// New Math Methods
-// New Number Properties
-// New Number Methods
-// New Global Methods
-// Object entries
-// JavaScript Modules
+// 1) The let keyword
+// 2) The const keyword
+// 3) TEMPLATE STRINGS
+// 4) Arrow Functions
+// 5) DESTRUCTURING
+// 6) OBJECT PROPERTIES
+// 7) DEFAULT ARGUMENTS
+// 8) REST OPERATORS
+// 9) SPREAD OPERATORS
+// 10) The ... Operator
+// 11) For/of
+// 12) Map Objects
+// 13) Set Objects
+// 14) Classes
+// 16) Promises
+// 17) Symbol
+// 18) Default Parameters   ------ Done
+
+// 19) Function Rest Parameter
+// 20) String.includes()
+// 21) String.startsWith()
+// 22) String.endsWith()
+// 23) Array.from()
+// 24) Array keys()
+// 25) Array find()
+// 26) Array findIndex()
+// 27) New Math Methods
+// 28) New Number Properties
+// 29) New Number Methods
+// 30) New Global Methods
+// 31) Object entries
+// 32) JavaScript Modules
 
 // 1) let and cost are already explain in ch1 but var is valid inside function(function scope) and let,const is valid in the declared block.
 
@@ -63,7 +64,7 @@ function mu(sd, dc) {
 console.log(mu(5)); // here we are providing the values to sd and dc but what if any of them get no value?
 // It returns NaN because no value was passed and number cannot be multiplied with empty value so Nan is returned.
 
-// In that case we assign a default value to or parameter like this - 
+// In the above case (line no 64), we assign a default value to or parameter like this - 
 function yamaha(mileage = 24, capacity = 12) {
     return mileage * capacity;
 }
@@ -72,10 +73,11 @@ console.log(yamaha()); // no value provided so default values will be printed.
 
 
 // 4) Arrow Function -
-// Arrow functions{()=>} are a clear and concise method
-// of writing normal/regular Javascript functions in a
-// more accurate and shorter way. Arrow functions were
-// introduced in the ES6 version.
+// Arrow functions{ 
+// '()=>' 
+// } are a clear and concise method of writing normal/regular Javascript functions 
+// in a ore accurate and shorter way. Arrow
+// functions were introduced in the ES6 version.
 // They make our code more structured and readable.  Example -
 
 const average = () => { // fuction is decalred and initialized.
