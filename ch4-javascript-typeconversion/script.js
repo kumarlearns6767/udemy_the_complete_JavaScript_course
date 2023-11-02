80cafd07aefd9352e67d189b30726bff2b9f6d2e
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-02 16:38:20 +0530

    Added the defination of type conversion in javascript

--- ch4-javascript-typeconversion/script.js
+++ ch4-javascript-typeconversion/script.js
@@ -2,4 +2,9 @@
 
 // Type Conversion IN JAVASCRIPT - 
 
+// What is Type Conversion in Javascript?
+
+//As the name implies, type conversion is the process of converting a value from one type to another. Values in JavaScript can be of different types. You could have a number, string, object, boolean – you name it. Sometimes, you may want to convert data from one type to another to fit a certain operation.
+
 // Javascript converts any data taken from outside(user input) to String Data Type. It is done automaticall and by default. This is one of the reason Type Conversion is required in Javascript to perfrom the things/operation/tasks we want to do in Javascript. 
+
