fa9afa8a688363fe84623a4b4a46c5ddf779c8b9
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-02 16:42:29 +0530

    explained the difference between typeof and typeof()

--- ch4-javascript-typeconversion/script.js
+++ ch4-javascript-typeconversion/script.js
@@ -2,9 +2,60 @@
 
 // Type Conversion IN JAVASCRIPT - 
 
+
 // What is Type Conversion in Javascript?
 
 //As the name implies, type conversion is the process of converting a value from one type to another. Values in JavaScript can be of different types. You could have a number, string, object, boolean – you name it. Sometimes, you may want to convert data from one type to another to fit a certain operation.
 
 // Javascript converts any data taken from outside(user input) to String Data Type. It is done automaticall and by default. This is one of the reason Type Conversion is required in Javascript to perfrom the things/operation/tasks we want to do in Javascript. 
+
+
+// What is typeof and typeof() in Javscript?
+
+// typeof is an operator in javascript who returns the type of any datatype where as typeof() is a function who returns the datatype of the variable/anything passed between the () of the typeof fucntion. Both does the same task but Use cases are different. For example, 
+
+// Here are some tips on when to use typeof as an operator and as a function:
+// Use typeof as an operator when:
+
+//     You want to determine the type of a value without calling a function.
+//     You want to improve the readability of your code by avoiding unnecessary parentheses.
+
+// Use typeof as a function when:
+
+//     You need to pass the type of a value to another function as an argument.
+//     You are working with a legacy codebase that uses typeof as a function.
+
+// Google coding standards
+
+// The Google JavaScript Style Guide recommends using typeof as a function with parentheses. This is to avoid ambiguity and to improve the readability of code.
+
+// Coding standards of other top 1% companies
+
+// Other top 1% companies, such as Airbnb and Netflix, also recommend using typeof as a function with parentheses.
+// Sure. Here are some tips on when to use typeof as an operator and as a function:
+
+// Use typeof as an operator when:
+
+//     You want to determine the type of a value without calling a function.
+//     You want to improve the readability of your code by avoiding unnecessary parentheses.
+
+// Use typeof as a function when:
+
+//     You need to pass the type of a value to another function as an argument.
+//     You are working with a legacy codebase that uses typeof as a function.
+
+// Google coding standards
+
+// The Google JavaScript Style Guide recommends using typeof as a function with parentheses. This is to avoid ambiguity and to improve the readability of code.
+
+// Coding standards of other top 1% companies
+
+// Other top 1% companies, such as Airbnb and Netflix, also recommend using typeof as a function with parentheses.
+
+// Overall
+
+// It is generally best to use typeof as a function with parentheses, unless you have a specific reason to do otherwise. This will help to improve the readability and maintainability of your code.
+// Overall
+
+// It is generally best to use typeof as a function with parentheses, unless you have a specific reason to do otherwise. This will help to improve the readability and maintainability of your code.
 
