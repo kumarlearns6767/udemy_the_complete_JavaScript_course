13a64dd65b9f8202351ad8c5aa91cac2e774ce03
Lalit Kumar Thakur <45649399+lalitkumarthakur@users.noreply.github.com>
2023-11-07 11:23:09 +0530

    added 'typeof operator' exmaple with defination

--- ch4-javascript-typeconversion/script.js
+++ ch4-javascript-typeconversion/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-// Type Conversion IN JAVASCRIPT - 
+// Type Conversion IN JAVASCRIPT -
 
-// Javascript converts any data taken from outside(user input) to String Data Type. It is done automaticall and by default. This is one of the reason Type Conversion is required in Javascript to perfrom the things/operation/tasks we want to do in Javascript. 
+// Javascript converts any data taken from outside(user input) to String Data Type. It is done automaticall and by default. This is one of the reason Type Conversion is required in Javascript to perfrom the things/operation/tasks we want to do in Javascript.
+
+// The type of a variable is identified by two thing:
+// 1) typeof operator
+// 2) typoof() function
+// Both works same, the only difference is One is operator and other is function and uses are based on scenarios/situations.
+
+// typeof operator -
+// Typeof in JavaScript is an operator used for type checking and returns the data type of the operand passed to it. The operand can be any variable, function, or object whose type you want to find out using the typeof operator.
+
+// Example - 
+let number = 12, nam = 'lalu', decesion = true, andnumber2 = 12.88;
+
+console.log(`typeof number = ${typeof number}, typeof nam = ${typeof nam}, typeof decesion = ${typeof decesion}, typeof andnumber2 = ${typeof andnumber2}`);
+
+// In above example we demostrated that how 'typeof operator' is used to identify the type of a variable.
