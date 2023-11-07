0a3e99131ab719d5189058e43bd1710e968ea555
lalitkumarthakur <lkt929129@gmail.com>
2023-11-07 19:18:34 +0530

    Added more about typr conversion

--- ch4-javascript-typeconversion/script.js
+++ ch4-javascript-typeconversion/script.js
@@ -31,10 +31,22 @@ console.log(`typeof number = ${typeof (numbe)}, typeof nam = ${typeof (namm)}, t
 // When we do any operation and if javascript is capable to understand that and needs to convert type of datatype in order to complete the operation then, javascript "automatically converts type of that datatype using it's intelligence". This is called "Automatic Type conversion".
 
 // Example - 
-
+// 1st example - 
 let conversion = 5 + null;  // how null is converted from onject to number datatype ?
 console.log(`Final result after Automatic Type conversion  = ${conversion}`);
 
 // In the above example, "5" is added to "null" which is not possible technically because null means nothing and this will throw error in other programming languages but here in Javascript, it converts "null" to "0" so, 5+0 = 5;
 
-alert(typeof null);  
+console.log(typeof null);   // To check the default datatype of null. 
+
+// 2nd example - 
+let con2 = "5" + null;
+console.log(`Final result after 2nd Automatic Type conversion = ${con2}`);
+
+// In the above example, ' "5" ' is added to "null" which is not possible technically because null means nothing and this will throw error in other programming languages but here in Javascript, it converts "null" to String datatype so, 5+null = 5null;
+
+// 3nd example - 
+let con3 = "5" - 11;
+console.log(`Final result after 2nd Automatic Type conversion = ${con3}`);
+
+// In the above example, ' "5" ' is substracted to 11 which is not possible technically because "5" is string and 11 is number and "5" must be converted to number to do that and this will throw error in other programming languages but here in Javascript, it converts "5" to Number datatype which is called "type coercion".
