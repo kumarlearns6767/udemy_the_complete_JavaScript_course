f87b2f1853aa468322c6ee46592b3251330c39d8
lalitkumarthakur <lkt929129@gmail.com>
2023-12-25 15:45:45 +0530

    fixed folder structure for mini-projects

--- ch13-javascript-DOM-&BOM/script.js
+++ ch13-javascript-DOM-&BOM/script.js
@@ -3,3 +3,11 @@
 // content taken from "chai aur code youtube channel"
 
 // Javascript DOM (Document Object Model) and BOM (Browser Object Model) -
+
+const execute = () => {
+	let name = "raj";
+	let name2 = name;
+	console.log(name2);
+};
+
+execute();
