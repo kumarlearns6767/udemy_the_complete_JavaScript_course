6003f086be4aade6adf4dcf8c51dbd181aa5906b
lalitkumarthakur <lkt929129@gmail.com>
2023-12-28 19:32:13 +0530

    fixed folder structure

---
+++ mini-projects/weather-app/README.md
@@ -0,0 +1,5 @@
+# weatherapp.github.io
+ A Weather App using Rapid API to know and experience API Building from the youtubr channel "GreatStack" and 
+ implenent Html, CSS, JavaScaript, API and bootstrap to build up my skills and goin experience.
+
+ #Project-1
---
+++ mini-projects/weather-app/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// This is the entry point for this Weather app.
+
+alert(
+	"This Weather App uses Open Weather API which says they collect your \
+data. Lalit does not stores your personal data nor \
+sells it in anyway. \n\nSince, Open Weather API is required to make this app work so, lalit has to use this API.\
+ Please review privacy policy of Open Weather API for more details."
+);
