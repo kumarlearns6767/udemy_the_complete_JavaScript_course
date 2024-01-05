6d598f9599af3cbe56a0b1b2e326af4cf20da937
lalitkumarthakur <lkt929129@gmail.com>
2024-01-05 15:09:17 +0530

    added 4 ways to write events

--- ch14-javascript-events/script.js
+++ ch14-javascript-events/script.js
@@ -35,6 +35,71 @@ const calledFunction = () => {
 	);
 };
 // Now this function is called in html onclick attribute when the user clicks on the button.
+// Method 2 is not suitable approach because when application i amde scalable in pure javascript, it creates a lot of problems.
 
 // Method 3 -
 //In this method, we select the id present in html, store them in javascript then do some event ction on that id. Example -
+
+// At first we select then store the id below -
+
+const medthod3id = document.querySelector("#method3");
+
+medthod3id.onclick = () => {
+	alert(
+		"This event is fired by selecting html id and applying onclick event of it"
+	);
+};
+
+// The problem comees with Method 3 is that it overwrites the current code and does not execute the code written before the last code. Example -
+
+medthod3id.onclick = () => {
+	console.log(
+		"This event is fired by selecting html id and applying onclick event of it"
+	);
+};
+
+// Check the console output, you will see the alert() is not executed but the last code of method 3 "console.log" is executed. So this is not idea for situation where we need to execute multiple code on single event click.
+// This method is also not suitable because onlick come with limited functionality and does not retuen many information which is required for working with advanced applications.
+
+// Method 4 -
+// We can use event listner like addEvetListener() to lstner the event and when the event is fired, it calls a call back function to execute it's intended code. Example -
+
+const method4id = document.querySelector(`#method4`);
+
+method4id.addEventListener(
+	"click",
+	() => {
+		alert(" Hello, I am called using addEventListner()");
+	},
+	false
+);
+
+// The Method 3 problem is solved by Method 4 so this is idea for the above situation showed in Method 3.
+
+// Method 4 is the ideal method as it come with more functionality and is suitable for sca;able applications amde in pure / vanilla javascript.
+
+// We need to lean the following onjects return by addEventListener() when a event is triggered -
+// type, timestamp, defaultPrevented,
+// target, toElement, srcRlement (very immportant), currentTarget,
+// clientX, clientY, screenY,
+// altKey, ctrlKey, shiftKey, keyCode
+
+// Event Propogation / Bubbling (very immportant) -
+// Event Propogation / Bubbling means that when the element is clicked then the events present on that element including any parent element's elements are executed in inner to outher format. Which means the child element's event will trigger first then the parent's avant will trigger. Example -
+
+document.querySelector("#method5").addEventListener(
+	"click",
+	() => {
+		console.log(" This is inner element's event triggered");
+	},
+	false
+);
+
+document.querySelector("#dContBody").addEventListener(
+	"click",
+	() => {
+		console.log(" This is parent element's event triggered");
+	},
+	false
+);
+// this is the demo of inner to outer execution which is event propogation.
