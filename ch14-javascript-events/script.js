180d4c09b5481477e70ad471db8c93ec112b3886
lalitkumarthakur <lkt929129@gmail.com>
2024-01-05 13:45:25 +0530

    added 2 methods(examples) to write events

--- ch14-javascript-events/script.js
+++ ch14-javascript-events/script.js
@@ -18,3 +18,23 @@
 // To react to an event, you attach an event handler to it. This is a block of code (usually a JavaScript function that you as a programmer create) that runs when the event fires. When such a block of code is defined to run in response to an event, we say we are registering an event handler. Note: Event handlers are sometimes called event listeners — they are pretty much interchangeable for our purposes, although strictly speaking, they work together. The listener listens out for the event happening, and the handler is the code that is run in response to it happening.
 
 // The above defination is taken from MDN Docs.
+
+// The are 4 ways to write events in Javascript -
+
+// 1) Method 1-
+// calling functions like alert() or user denifed function which is linked to html as inline. Example -
+
+// see index.html of ch14 -  events line no 37.
+
+// Method 2-
+// by calling functtion written in javascript. Example -
+
+const calledFunction = () => {
+	alert(
+		` This is 2nd method in which I am called and my name is "calledDunction"`
+	);
+};
+// Now this function is called in html onclick attribute when the user clicks on the button.
+
+// Method 3 -
+//In this method, we select the id present in html, store them in javascript then do some event ction on that id. Example -
